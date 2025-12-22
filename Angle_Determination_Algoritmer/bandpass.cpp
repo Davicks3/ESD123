ff3721871219c94ca6582c58c8b34dbca3727d1d
Davicks3 <davicks33@gmial.com>
2025-12-22 03:53:08 +0100

    rydderop2

--- Angle_Determination_Algoritmer/bandpass.cpp
+++
@@ -1,9 +0,0 @@
-#include "bandpass.h"
-
-Bandpass::Bandpass() {}
-
-void Bandpass::filter() {}
-
-void Bandpass::lowpass_filter() {}
-
-void Bandpass::highpass_filter() {}
--- Angle_Determination_Algoritmer/globals.h
+++
@@ -1,3 +0,0 @@
-#define V_SOUND     343.0
-#define PI          3.14159265358979323846
-#define TWO_PI      (2.0 * PI)
